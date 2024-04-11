58534601c64d8abe9e2f004a51a05e5b67eb812a
Loïc-Jonathan BIAKOU <loic-jonathan.biakou@exakis-nelite.com>
2024-04-11 23:48:57 +0200

    update

--- script.ps1
+++ script.ps1
@@ -1,1 +1,6 @@
+# Configure WinRM for Ansible
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ansible/ansible-documentation/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
+
+# Disable all firewall profiles
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
